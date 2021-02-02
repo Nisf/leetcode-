40ba99dd49fb6e717294f67bd41e46e2b1e9d4c6
nsf_global <nsf_global@qq.com>
2021-02-02 20:54:25 +0800

    1. 0-1背包 2. BFS

--- lc-279.cpp
+++ lc-279.cpp
@@ -1,4 +1,5 @@
-﻿class Solution {
+﻿// 0-1背包
+class Solution {
 public:
     int numSquares(int n) {
         vector<int> coins;
@@ -14,5 +15,32 @@ public:
             for (int j = coins[i]; j <= n; j ++ )
                 f[j] = min(f[j], f[j - coins[i]] + 1);
         return f[n];
+    }
+};
+
+// BFS
+class Solution {
+public:
+    int numSquares(int n) {
+        queue<int> q;
+        unordered_set<int> hash;
+        q.push(n); hash.insert(n);
+
+        int cnt = 0;
+        while (q.size()) {
+            cnt ++ ;
+            int level = q.size();
+            while (level -- ) {
+                int t = q.front();
+                q.pop();
+                for (int i = 1; i * i <= t; i ++ ) {
+                    int res = t - i * i;
+                    if (res == 0) return cnt;
+                    q.push(res);
+                    hash.insert(res);
+                }
+            }
+        }
+        return cnt;
     }
 };
