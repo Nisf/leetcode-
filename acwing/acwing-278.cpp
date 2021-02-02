f31c7b6c4f12495d0a984921742f39d88823e35c
nsf_global <nsf_global@qq.com>
2021-02-02 23:38:38 +0800

    数字组合（背包恰好装满，注意状态的初始化）

---
+++ acwing/acwing-278.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+const int N = 10010;
+
+int f[N];
+
+int n, m;
+
+int main()
+{
+    cin >> n >> m;
+    
+    f[0] = 1; //背包恰好装满
+    for (int i = 0; i < n; i ++ )
+    {
+        int v;
+        cin >> v;
+        for (int j = m; j >= v; j -- )
+            f[j] += f[j - v];
+    }
+    
+    cout << f[m] << endl;
+    
+    return 0;
+}
