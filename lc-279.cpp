ab43f3b0d7a579e0eee643d2543381ff2e8beca4
nsf_global <nsf_global@qq.com>
2021-02-02 20:33:54 +0800

    完全平方数(0-1背包)

---
+++ lc-279.cpp
@@ -0,0 +1,18 @@
+﻿class Solution {
+public:
+    int numSquares(int n) {
+        vector<int> coins;
+        for (int i = 1; i <= n; i ++ ) {
+            int t = sqrt(i);
+            if (t * t == i) {
+                coins.push_back(i);
+            }
+        }
+        vector<int> f(n + 1, 1e8);
+        f[0] = 0; //别忘了初始化
+        for (int i = 0; i < coins.size(); i ++ )
+            for (int j = coins[i]; j <= n; j ++ )
+                f[j] = min(f[j], f[j - coins[i]] + 1);
+        return f[n];
+    }
+};
