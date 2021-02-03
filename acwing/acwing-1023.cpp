752d83a352d110ef4bd906705e0cf6d68908a487
nsf_global <nsf_global@qq.com>
2021-02-03 11:56:55 +0800

    完全背包（背包恰好装满）

---
+++ acwing/acwing-1023.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+const int N = 1010;
+
+int f[N];
+int v[4] = {10, 20, 50, 100};
+
+int main()
+{
+    int m;
+    cin >> m;
+    f[0] = 1; //初始化
+    
+    for (int i = 0; i < 4; i ++ )
+        for (int j = v[i]; j <= m; j ++ )
+            f[j] += f[j - v[i]];
+    cout << f[m] << endl;
+    return 0;
+}
